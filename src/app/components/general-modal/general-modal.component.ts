0fdb0a9c5008f879d08d23a2c8df63b3b8775c97
RicardoHungJS <ricardohung92@gmail.com>
2024-12-15 14:17:33 -0500

    first commit

---
+++ src/app/components/general-modal/general-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-modal',
+  imports: [],
+  templateUrl: './general-modal.component.html',
+  styleUrl: './general-modal.component.scss'
+})
+export class GeneralModalComponent {
+
+}
